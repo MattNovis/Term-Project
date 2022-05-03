390564c234bccf286c0edfc589f031c1e6524176
kstokes92 <kds1277@mavs.uta.edu>
2022-05-02 20:01:51 -0500

    Added about us page

---
+++ Website/about.php
@@ -0,0 +1,25 @@
+<?php
+    include ('shared.php');
+?>
+
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+        <meta name="keywords" content="keyword">
+        <meta name="description" content="About page contaning skills for Matthew Novis.">
+        <meta name="Matthew Novis" content="About Matthew Novis Page">
+    <title>Form</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/4981312403.js" crossorigin="anonymous"></script>
+</HEAD>
+
+<BODY>
+    <?php echo $header ?>
+
+
+    <?php echo $footer ?>
+
+
+</BODY>
+</HTML>
